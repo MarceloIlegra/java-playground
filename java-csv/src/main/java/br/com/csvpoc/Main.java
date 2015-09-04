45223bf5d359c97b1a806605e730d968413bc583
Marcelo Ilegra <marcelo.serpa@ilegra.com>
2015-09-04 16:49:31 -0300

    Refatorado

--- java-csv/src/main/java/br/com/csvpoc/Main.java
+++ java-csv/src/main/java/br/com/csvpoc/Main.java
@@ -1,44 +1,17 @@
 package br.com.csvpoc;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 
 public class Main {
 
-    private static final String NEW_LINE_SEPARATOR = "\n";
-    private static final Object[] FILE_HEADER = {"email", "partner"};
-
     public static void main(String[] args) {
-        FileWriter fileWriter = null;
-        CSVPrinter csvFilePrinter = null;
-        CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+        ModelWriterCSV modelWriterCSV = new ModelWriterCSV("arquivofinal");
+        modelWriterCSV.generateFile(generateModelsFake());
+    }
 
-
-        List<Model> models = Arrays.asList(new Model("email@email.com", "email"), new Model("aaaa@aaaa.com", "ddsdsds"), new Model("444@email.com", "afafdsfds"));
-
-        try {
-            fileWriter = new FileWriter("arquivofinal.csv");
-            csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat);
-            csvFilePrinter.printRecord("sep=,");
-            csvFilePrinter.printRecord(FILE_HEADER);
-
-            for(Model model : models){
-                csvFilePrinter.printRecord(Arrays.asList(model.getEmail(), model.getPartner()));
-            }
-
-            fileWriter.flush();
-            fileWriter.close();
-            csvFilePrinter.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
+    private static List<Model> generateModelsFake() {
+        return Arrays.asList(new Model("email@email.com", "email"), new Model("aaaa@aaaa.com", "ddsdsds"), new Model("444@email.com", "afafdsfds"));
     }
 
 }
---
+++ java-csv/src/main/java/br/com/csvpoc/ModelWriterCSV.java
@@ -0,0 +1,39 @@
+
+package br.com.csvpoc;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+public class ModelWriterCSV {
+
+    private final String NEW_LINE_SEPARATOR = "\n";
+    private final String COLUMN_SEPARATOR = "sep=,";
+    private final Object[] FILE_HEADER = {"email", "partner"};
+    private final CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+    private String filename;
+
+    public ModelWriterCSV(String filename){
+        this.filename = filename;
+    }
+
+    public void generateFile(List<Model> models){
+        try(FileWriter fileWriter = new FileWriter(filename+".csv"); CSVPrinter csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat)) {
+            csvFilePrinter.printRecord(COLUMN_SEPARATOR);
+            csvFilePrinter.printRecord(FILE_HEADER);
+            models.stream().forEach( m -> {
+                try {
+                    csvFilePrinter.printRecord(Arrays.asList(m.getEmail(), m.getPartner()));
+                } catch (IOException ex) {
+                    throw new RuntimeException("Cannot write in the file");
+                }
+            });
+            fileWriter.flush();
+        } catch (IOException ex) {
+            throw new RuntimeException("Cannot create the file");
+        }
+    }
+}
