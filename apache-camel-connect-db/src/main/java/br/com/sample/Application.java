fc97848eda0e5c6d55d1807df41f680e9af0f49f
Marcelo Ilegra <marcelo.serpa@ilegra.com>
2016-03-24 18:08:31 -0300

    acessing h2 database

--- apache-camel-connect-db/src/main/java/br/com/sample/Application.java
+++ apache-camel-connect-db/src/main/java/br/com/sample/Application.java
@@ -5,19 +5,34 @@ import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
 import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Application {
 
     public static void main(String[] args) throws Exception {
 
-        CamelContext camelContext = new DefaultCamelContext();
-
         BasicDataSource basicDataSource =  new BasicDataSource();
-        //basicDataSource.setDriver();
-        basicDataSource.setUrl("");
-        basicDataSource.setUsername("");
+        basicDataSource.setDriverClassName("org.h2.Driver");
+        basicDataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+        basicDataSource.setUsername("sa");
         basicDataSource.setPassword("");
+
+
+        PreparedStatement preparedStatement = basicDataSource.getConnection().prepareStatement("select * from teste");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("DESCRIPTION"));
+        }
+        
+        SimpleRegistry reg = new SimpleRegistry() ;
+        reg.put("myDataSource",basicDataSource);
+
+        CamelContext camelContext = new DefaultCamelContext(reg);
         /**
          * <property name="driverClassName" value="org.apache.derby.jdbc.EmbeddedDriver"/>
          <property name="url" value="jdbc:derby:memory:orders;create=true"/>
@@ -25,7 +40,7 @@ public class Application {
          <property name="password" value=""/>
          */
 
-        camelContext.addService(basicDataSource);
+        //camelContext.addService(basicDataSource);
         camelContext.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
